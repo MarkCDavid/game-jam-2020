1503b7779162608a2c404b38099dd128f0cc13f2
MarkCDavid <sakalysaurimas@gmail.com>
2020-02-01 01:00:42 +0200

    post processing and graphics

--- Assets/Scripts/Issue.cs
+++ Assets/Scripts/Issue.cs
@@ -6,6 +6,12 @@ using UnityEngine;
 public class Issue : MonoBehaviour
 {
     public Action OnFixed;
+    public Action OnCritical;
+    
+    public float CriticalTime;
+
+    private bool _isCritical = false;
+    private float _currentLifeTime = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,5 +21,17 @@ public class Issue : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        _currentLifeTime += Time.deltaTime;
+        if (!_isCritical && _currentLifeTime > CriticalTime)
+        {
+            _isCritical = true;
+            OnCritical?.Invoke();
+        }
+    }
+
+
+    private void FixIssue()
+    {
+        
     }
 }
