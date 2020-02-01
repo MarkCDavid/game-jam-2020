1c5337fc59b635ae639b09fe34ec8bc0768316d5
MarkCDavid <sakalysaurimas@gmail.com>
2020-02-01 18:22:57 +0200

    fad

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,8 +7,8 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-//        Cursor.lockState = CursorLockMode.Locked;
-//        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
