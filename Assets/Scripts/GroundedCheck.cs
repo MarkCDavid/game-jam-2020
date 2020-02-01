b47bd61c7a8474eb46c5383d3f52e40b5d9e87c1
MarkCDavid <sakalysaurimas@gmail.com>
2020-02-01 11:56:45 +0200

    little fixes to movement

---
+++ Assets/Scripts/GroundedCheck.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundedCheck : MonoBehaviour
+{
+    public bool IsGrounded = true;
+
+    private void OnCollisionStay(Collision other)
+    {
+        if (other.transform != transform.parent)
+            IsGrounded = true;
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -12,52 +12,48 @@ public class Movement : MonoBehaviour
     Rigidbody rb;
     public Vector3 jump;
     public float JumpForce = 3.5f;
-    public bool IsGrounded = true;
     public float speed = 5f;
     private Transform camera;
-    private int JumpTime=0;
 
+    private GroundedCheck gc;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
         camera = GetComponentInChildren<Camera>().transform;
         jump = new Vector3(0.0f, 2.0f, 0.0f);
+        gc = GetComponentInChildren<GroundedCheck>();
     }
 
 
-    void OnCollisionStay()
-    {
-        IsGrounded = true;
-    }
+    
     void Update()
     {
-        var forward = new Vector3(camera.forward.x, 0, camera.forward.z).normalized;
-        var right = new Vector3(camera.right.x, 0, camera.right.z).normalized;
-        if (JumpTime > 0) JumpTime--;
-        if (Input.GetKey("a"))
+
+        var movementDelta = Vector3.zero;
+        
+        
+        if (Input.GetKey(KeyCode.A))
+            movementDelta += Time.deltaTime * speed * -CameraRight;
+        if (Input.GetKey(KeyCode.D))
+            movementDelta += Time.deltaTime * speed * CameraRight;
+        if (Input.GetKey(KeyCode.S))
+            movementDelta += Time.deltaTime * speed * -CameraForward;
+        if (Input.GetKey(KeyCode.W))
+            movementDelta += Time.deltaTime * speed * CameraForward;
+
+        transform.position += Time.deltaTime * speed * movementDelta.normalized;
+        
+        
+        if (Input.GetKey(KeyCode.Space)&& gc.IsGrounded)
         {
-            transform.position += -right * speed * Time.deltaTime;
-        }
-        if (Input.GetKey("d"))
-        {
-            transform.position += right * speed * Time.deltaTime;
-        }
-        if (Input.GetKey("s"))
-        {
-            transform.position += -forward * speed * Time.deltaTime;
-        }
-        if (Input.GetKey("w"))
-        {
-            transform.position += forward * speed * Time.deltaTime;
-        }
-        if (Input.GetKey("space")&&IsGrounded&&JumpTime==0)
-        {
-            JumpTime = 50;
-            IsGrounded = false;
+            gc.IsGrounded = false;
             rb.AddForce(jump * JumpForce, ForceMode.Impulse);
         }
     }
+    
+    private Vector3 CameraForward => new Vector3(camera.forward.x, 0, camera.forward.z).normalized;
+    private Vector3 CameraRight => new Vector3(camera.right.x, 0, camera.right.z).normalized;
 
 
     
