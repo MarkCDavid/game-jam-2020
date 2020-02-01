5eefca9db9f4cc70886d2a703a087d1f9f5f41be
Mew <j.kuskys1@gmail.com>
2020-02-01 13:17:24 +0200

    Height and Pitch scripts added

---
+++ Assets/Scripts/Height.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Height : MonoBehaviour
+{
+    public float HeightTarget = 0;
+    public float DecentSpeed = 0f;
+    public Action OnPlaneFall;
+
+    void SpeedChange(float Severity) {
+        DecentSpeed  = Severity;        
+    }
+    void Update()
+    {      
+        transform.position = new Vector3(transform.position.x, transform.position.y+DecentSpeed, transform.position.z);
+        if (transform.position.y < HeightTarget)
+            OnPlaneFall?.Invoke();
+    }
+}
