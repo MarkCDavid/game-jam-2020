c0a17039edd202e5262ad15f9bd4d4411286d7aa
MarkCDavid <sakalysaurimas@gmail.com>
2020-02-02 14:31:44 +0200

    final

---
+++ Assets/Scripts/HeightSlider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeightSlider : MonoBehaviour
+{
+    private Slider slider;
+    public Transform height;
+
+    private void Awake()
+    {
+        slider = GetComponentInChildren<Slider>();
+    }
+
+    void Update()
+    {
+        slider.value = height.position.y / 100;
+    }
+}
